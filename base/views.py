2eb87ebc0e4cd2c4743c1cce3c8f7e7ca75c87ab
P-Horus <feranmia51@gmail.com>
2023-01-08 00:59:06 +0100

    updated user add items

--- base/views.py
+++ base/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import *
 
 # Create your views here.
 
 def homePage(request):
     tasks = Task.objects.all()
-    context = {'tasks':tasks}
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid:
+            form.save()
+        return redirect('/')
+    context = {'tasks':tasks, 'form':form}
     return render(request, 'base/list.html', context)
